43b60b7bdfa40b21c6e0fe9eb2d07e37dff6cc37
desaigner <sai@desaign.me>
2018-09-17 08:00:45 -0500

    Task.js updated

--- client/src/components/Projects/Task.js
+++ client/src/components/Projects/Task.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 import axios from "axios";
+import TaskForm from "./tasks";
 
 class Task extends Component {
   render() {
     const theTask = this.props.data;
     return (
-      <div>
-        {theTask.taskName}
-        <br />
-        {theTask.taskDescription}
+      <div class="taskcard">
+        <p>
+          <h5>{theTask.taskName}</h5>
+          Date of Task: {new Date(theTask.taskDate).toDateString()}
+          <br />
+          Rate per Hour: ${theTask.taskRate}
+          <br />
+          Hours Worked: {theTask.taskHours}
+          <br />
+          Total Earned from Task: ${theTask.taskRate * theTask.taskHours}
+          <br />
+          Edit Task / Delete Task
+        </p>
       </div>
     );
   }
