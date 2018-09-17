3feffda516c2ce1a93c9d296431e19fb3b6b1528
desaigner <sai@desaign.me>
2018-09-17 07:56:19 -0500

    Project.js updated

--- client/src/components/Projects/Project.js
+++ client/src/components/Projects/Project.js
@@ -1,47 +1,42 @@
 import React, { Component } from "react";
 import axios from "axios";
-import Task from "./Task";
+import Task from "./task";
 
 class Project extends Component {
   render() {
     const aProject = this.props.data;
+    console.log(this.props.handleExpand);
     return (
-      <div>
-        {/* <h3>{aProject.projectTitle}</h3>
-        {aProject.tasks.map(t => (
-          <Task data={t} />
-        ))} */}
-        <ul className="list-group">
-          <li className="list-group-item d-flex justify-content-between align-items-center">
-            {aProject.projectTitle}
-            <div className="projectCard">
-                    {aProject.tasks.map(aTask => {
-                      return (
-                        <div>
-                          {aTask.taskName} <br /> {aTask.taskDescription}
-                        </div>
-                      );
-                    })}
-                    <button>View</button>
-                    <button>Delete</button>
-                  </div>
-          </li>
-        </ul>
-        {/* <p>{aProject.projectTitle}</p>
-          <div className="projectCard">
-            {aProject.tasks.map(aTask => {
-              return (
-                <div>
-                  {aTask.taskName} <br /> {aTask.taskDescription}
-                </div>
-              );
-            })}
-
-            <button>View/Add Project Details</button>
-            <p>
-              <button>Delete Project</button>
-            </p>
-          </div> */}
+      <div className="projectCard">
+        <h3 class="titlecard">
+          {aProject.projectTitle}
+          <span className="projectDate">
+            <span>Created </span>
+            {new Date(aProject.projectDate).toDateString()}
+          </span>
+        </h3>
+        <h5 class="budgetcard">
+          Estimated Budget: ${aProject.projectBudget}
+          <br /> Actual Budget: ${aProject.actualBudget}
+        </h5>
+        <div class="taskinfo">
+          {this.props.expand
+            ? aProject.tasks.map(t => <Task data={t} />)
+            : null}
+        </div>
+        <button
+          class="taskbutton icons btn btn-primary"
+          onClick={() => this.props.handleExpand(aProject._id)}
+        >
+          Show Tasks
+        </button>
+        <button
+          class="taskbutton icons btn btn-primary"
+          onClick={event => this.props.handleAddTask(event, aProject._id)}
+        >
+          Add Task
+        </button>
+        <button class="taskbutton icons btn btn-primary">Delete Poject</button>
       </div>
     );
   }
